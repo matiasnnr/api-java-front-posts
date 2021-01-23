b68bed6fccb155b63bae1c96c38af8c83ba9af88
matiasnnr <matias.nnr@hotmail.com>
2021-01-23 18:56:28 -0300

    add: decoded token & put it on global state and localStorage

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
+import jwtDecode from 'jwt-decode';
 import { LOGIN_ENDPOINT } from '../helpers/endpoints';
+import { SET_CURRENT_USER } from './types';
+import setAuthToken from '../helpers/setAuthToken';
 
 export const loginUser = (userData) => dispatch => {
 
@@ -12,9 +15,27 @@ export const loginUser = (userData) => dispatch => {
 
             console.log(response);
 
+            const { authorization, userid } = response.headers;
+
+            localStorage.setItem('jwtToken', authorization);
+
+            // func para añadir token a axios
+            setAuthToken(authorization);
+
+            const decoded = jwtDecode(authorization);
+
+            dispatch(setCurrentUser({ user: decoded, loggedIn: true }));
+
             resolve(response);
         }).catch(error => {
             reject(error);
         })
     })
 }
+
+export const setCurrentUser = ({ user, loggedIn }) => { // con esto el usuario decodificado desde el token, será puesto en el state global de la app
+    return {
+        type: SET_CURRENT_USER,
+        payload: { user, loggedIn }
+    }
+};
