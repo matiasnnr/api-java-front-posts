22b5287b8edc496ffe31d3120561f06607b4aeaa
matiasnnr <matias.nnr@hotmail.com>
2021-01-25 18:42:35 -0300

    add: protected routes by auth

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import SignIn from "./pages/SignIn";
 import store from './store';
 import { Provider } from 'react-redux';
 import checkForToken from "./helpers/checkForToken";
+import PrivateRoute from "./utils/PrivateRoute";
+import UserPosts from "./pages/UserPosts";
 
 checkForToken();
 
@@ -23,6 +25,7 @@ function App() {
           <Switch>
             <Route exact path="/" component={Posts}></Route>
             <Route exact path="/signin" component={SignIn}></Route>
+            <PrivateRoute exact path="/posts" component={UserPosts} ></PrivateRoute>
           </Switch>
         </Container>
       </Router>
